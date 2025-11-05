6cbee6e52a052fd749c812062a9479db98a8d3f4
James Gill <echoing1822tide@outlook.com>
2025-11-05 09:41:27 -0700

    File Location Changed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿namespace Inspire_Me;
+namespace MSSA_CCAD21_C_InspireMe;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
     }
 }
